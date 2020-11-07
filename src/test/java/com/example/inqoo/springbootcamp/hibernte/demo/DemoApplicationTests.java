045a5f6bd695220f1d46df3384eb98f53d919dda
karodop <carolinnee.wit@gmail.com>
2020-11-07 14:15:59 +0100

    first commit

---
+++ src/test/java/com/example/inqoo/springbootcamp/hibernte/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inqoo.springbootcamp.hibernte.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
