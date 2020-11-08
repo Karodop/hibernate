8caaa08291c60fa6f80d26f2dc7d1b2aed6c7839
karodop <carolinnee.wit@gmail.com>
2020-11-08 11:22:58 +0100

    one to one

--- src/main/java/hibernateRelations/Adress.java
+++ src/main/java/hibernateRelations/Adress.java
@@ -1,13 +1,40 @@
 package hibernateRelations;
 
+import javax.persistence.*;
+
+@Entity
 public class Adress {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String city;
     private String streetName;
+    private int houseNr;
+    private String postalCode;
+
+
+    @OneToOne
+    private Person person;
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+
+    public Adress(String city, String streetName, int houseNr, String postalCode) {
+        this.houseNr = houseNr;
+        this.postalCode = postalCode;
+        this.city = city;
+        this.streetName = streetName;
+
+    }
 
     public Adress (){
-
     }
 
     public Long getId() {
@@ -34,11 +61,11 @@ public class Adress {
         this.streetName = streetName;
     }
 
-    public String getHouseNr() {
+    public int getHouseNr() {
         return houseNr;
     }
 
-    public void setHouseNr(String houseNr) {
+    public void setHouseNr(int houseNr) {
         this.houseNr = houseNr;
     }
 
@@ -50,16 +77,15 @@ public class Adress {
         this.postalCode = postalCode;
     }
 
-    public String getPerson() {
-        return person;
+    @Override
+    public String toString() {
+        return "Adress{" +
+                "id=" + id +
+                ", city='" + city + '\'' +
+                ", streetName='" + streetName + '\'' +
+                ", houseNr='" + houseNr + '\'' +
+                ", postalCode='" + postalCode + '\'' +
+                '}';
     }
-
-    public void setPerson(String person) {
-        this.person = person;
-    }
-
-    private String houseNr;
-    private String postalCode;
-    private String person;
 
 }
---
+++ src/main/java/hibernateRelations/AdressRepository.java
@@ -0,0 +1,6 @@
+package hibernateRelations;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdressRepository extends JpaRepository<Adress, Long> {
+}
